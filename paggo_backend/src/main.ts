dc572a2d2f797c23eb42ae2ae30bb9b8422e08ef
MurakamiHenrique <h.murakamidepaula@gmail.com>
2025-06-03 15:57:33 +0200

    Changing from sqlite to postgresql

--- paggo_backend/src/main.ts
+++ paggo_backend/src/main.ts
@@ -26,6 +26,7 @@ async function bootstrap() {  const app = await NestFactory.create(AppModule);
     credentials: true,
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
